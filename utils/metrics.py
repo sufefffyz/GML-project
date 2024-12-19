85c0756b6229926e9eae1db00e967d36ddbe3e26
sufefffyz <825521004@qq.com>
2024-12-19 18:31:46 +0800

    first commit

---
+++ utils/metrics.py
@@ -0,0 +1,38 @@
+# import torch
+import jittor as jt
+from sklearn.metrics import average_precision_score, roc_auc_score
+
+
+# def get_link_prediction_metrics(predicts: torch.Tensor, labels: torch.Tensor):
+def get_link_prediction_metrics(predicts: jt.Var, labels: jt.Var):
+    """
+    get metrics for the link prediction task
+    :param predicts: Tensor, shape (num_samples, )
+    :param labels: Tensor, shape (num_samples, )
+    :return:
+        dictionary of metrics {'metric_name_1': metric_1, ...}
+    """
+    predicts = predicts.detach().numpy()
+    labels = labels.numpy()
+
+    average_precision = average_precision_score(y_true=labels, y_score=predicts)
+    roc_auc = roc_auc_score(y_true=labels, y_score=predicts)
+
+    return {'average_precision': average_precision, 'roc_auc': roc_auc}
+
+
+# def get_node_classification_metrics(predicts: torch.Tensor, labels: torch.Tensor):
+def get_node_classification_metrics(predicts: jt.Var, labels: jt.Var):
+    """
+    get metrics for the node classification task
+    :param predicts: Tensor, shape (num_samples, )
+    :param labels: Tensor, shape (num_samples, )
+    :return:
+        dictionary of metrics {'metric_name_1': metric_1, ...}
+    """
+    predicts = predicts.detach().numpy()
+    labels = labels.numpy()
+
+    roc_auc = roc_auc_score(y_true=labels, y_score=predicts)
+
+    return {'roc_auc': roc_auc}
